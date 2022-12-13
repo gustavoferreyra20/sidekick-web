178f974e1b6ff95441832cb0a0ea3b1ba8b654b1
gustavoferreyra20 <ferreyragustavo847@gmail.com>
2022-12-13 20:04:36 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SideKick</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="img/logo.png" rel="icon" type="image/ico" />
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-custom navbar-dark fixed-top">
+  <a class="navbar-brand" href="#">
+      <img src="img/logo.png" width="30" height="30" alt="icon">
+  </a>
+      
+  <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ml-auto">
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#download">Descargar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Testimonios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sobre nosostros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacto</a>
+                    </li>
+                  </ul>               
+  </div>        
+</nav>
+
+    <img class="img-fluid" src="img/rainy-city-view-wallpaper.png" alt="rainy-city-view-wallpaper">
+
+    <div class="container my-5">
+
+        <div class="card mb-3 border-0"  style="width:800px; margin:0 auto;">
+          <div class="row no-gutters">
+
+            <div class="col-md-4">
+            <img class="img-fluid" alt="saving-time" src="img/saving-time.png" height="227" width="227">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <p class="card-text">Estas cansado de seguir perdiendo tiempo cuando tu juego favorito lo único que hace es cruzarte con jugadores que no están al mismo nivel que vos? <br><br> O que simplemente no les gusta hacer las cosas bien y el que termina pagando con su tiempo sos vos? <br> Con Sidekick esto no te sucederá nunca más!.</p>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <p class="card-text">Por medio de Sidekick conocerás gente nueva, con los mismos intereses que vos. <br><br> Te sorprenderá la cantidad de buenos momentos que podes compartir con un desconocido, solamente hay que saber donde buscar!.</p>
+              </div>
+            </div>
+            <div class="col-md-4">
+            <img class="img-fluid" alt="community" src="img/community.png" height="227" width="227">
+            </div>
+            <div class="col-md-4">
+            <img class="img-fluid" alt="karma" src="img/karma.png" height="227" width="227">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <p class="card-text">Con nuestro avanzado sistema de calificaciones que mezcla la Habilidad y el Karma podés tener una gran idea de lo que te vas a encontrar antes de invertir tu tan preciado tiempo. <br><br> Con Sidekick las sorpresas a la hora de jugar son cosa del pasado!.</p>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <p class="card-text">Además, con nuestro sistema de calificaciones premium y premios por performance, el buen juego y las buenas actitudes son siempre recompensadas! <br><br> Sidekick apunta a tener una base sobre el comportamiento de todos los jugadores, y al hacer esto, reducimos la cantidad de jugadores con una mala actitud en toda la comunidad gamer!.</p>
+              </div>
+            </div>
+            <div class="col-md-4">
+            <img class="img-fluid" alt="medal" src="img/medal.png" height="227" width="227">
+            </div>
+          </div>
+        </div>
+    </div>
+
+    <div id="download">
+    <img class="img-fluid" src="img/separator.png" alt="separator">
+      
+      <div class="card mb-3 border-0"  style="width:1500px; margin:0 auto;">
+        <div class="row no-gutters">
+
+
+          <div class="col-md-8">
+                <div class="card-body">
+                <h5 class="card-title">Usa Sidekick en cualquier dispositivo</h5>
+                  <p class="card-text">La aventura te espera. Pasa el rato con tus amigos en cualquier campo de batalla, sea en tu PC o dispositivo movil.</p>
+                </div>
+              </div>
+              <div class="col-md-4">
+              <img class="img-fluid" alt="medal" src="img/platforms.png" >
+              </div>
+          </div>
+        </div>
+
+        <div class="container my-5">
+        <div class="row">
+          <div class="card col-md-3 text-center">
+              <img class="card-img-top" src="img/windows-logo.png" alt="windows"  height="194" width="162">
+              <br>
+              <div class="card-body">
+                <a href="#" class="btn btn-primary">Descargar para Windows</a>
+              </div>
+            </div>
+
+            <div class="card col-md-3 text-center">
+              <img class="card-img-top" src="img/apple.png" alt="apple"  height="194" width="162">
+              <br>
+              <div class="card-body">
+                <a href="#" class="btn btn-primary">Descargar para iOS</a>
+              </div>
+            </div>
+
+            <div class="card col-md-3 text-center">
+              <img class="card-img-top" src="img/android.png" alt="android"  height="194" width="162">
+              <br>
+              <div class="card-body">
+                <a href="#" class="btn btn-primary">Descargar para Android</a>
+              </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    
+</body>
+</html>
