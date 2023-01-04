d5225fb01049f79416bfffac30cb4bf6307918ac
gustavoferreyra20 <ferreyragustavo847@gmail.com>
2023-01-04 14:35:37 -0300

    contact and design fixes

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles.css">
     <link href="img/logo.png" rel="icon" type="image/ico" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-md navbar-custom navbar-dark fixed-top">
@@ -26,62 +27,80 @@
                         <a class="nav-link" href="#download">Descargar</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#Info">Info</a>
+                        <a class="nav-link" href="#info">Info</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Contacto</a>
+                        <a class="nav-link" href="#contact">Contacto</a>
                     </li>
                   </ul>               
   </div>        
 </nav>
 
-    <img class="img-fluid" src="img/rainy-city-view-wallpaper.png" alt="rainy-city-view-wallpaper">
+    <img class="img-fluid bg top" src="img/rainy-city-view-wallpaper.png" alt="rainy-city-view-wallpaper">
 
     <div class="container my-5">
 
-        <div class="card mb-3 border-0">
-          <div class="row no-gutters">
 
-            <div class="col-md-4">
-            <img class="img-fluid" alt="saving-time" src="img/saving-time.png" height="227" width="227">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <p class="card-text">Estas cansado de seguir perdiendo tiempo cuando tu juego favorito lo único que hace es cruzarte con jugadores que no están al mismo nivel que vos? <br><br> O que simplemente no les gusta hacer las cosas bien y el que termina pagando con su tiempo sos vos? <br> Con Sidekick esto no te sucederá nunca más!.</p>
-              </div>
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <p class="card-text">Por medio de Sidekick conocerás gente nueva, con los mismos intereses que vos. <br><br> Te sorprenderá la cantidad de buenos momentos que podes compartir con un desconocido, solamente hay que saber donde buscar!.</p>
-              </div>
-            </div>
-            <div class="col-md-4">
-            <img class="img-fluid" alt="community" src="img/community.png" height="227" width="227">
-            </div>
-            <div class="col-md-4">
-            <img class="img-fluid" alt="karma" src="img/karma.png" height="227" width="227">
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <p class="card-text">Con nuestro avanzado sistema de calificaciones que mezcla la Habilidad y el Karma podés tener una gran idea de lo que te vas a encontrar antes de invertir tu tan preciado tiempo. <br><br> Con Sidekick las sorpresas a la hora de jugar son cosa del pasado!.</p>
-              </div>
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <p class="card-text">Además, con nuestro sistema de calificaciones premium y premios por performance, el buen juego y las buenas actitudes son siempre recompensadas! <br><br> Sidekick apunta a tener una base sobre el comportamiento de todos los jugadores, y al hacer esto, reducimos la cantidad de jugadores con una mala actitud en toda la comunidad gamer!.</p>
-              </div>
-            </div>
-            <div class="col-md-4">
-            <img class="img-fluid" alt="medal" src="img/medal.png" height="227" width="227">
+      <div class="card border-0">
+        <div class="row no-gutters">
+          <div class="col-xl-3 col-lg-3 col-md-12 col-xs-12">
+            <img class="img-fluid" alt="saving-time" src="img/save-time.jpg">
+          </div>
+          <div class="col-xl-9 col-lg-9 col-md-12 col-xs-12">
+            <div class="card-body">
+            <p class="card-text">Estas cansado de seguir perdiendo tiempo cuando tu juego favorito lo único que hace es cruzarte con jugadores que no están al mismo nivel que vos? <br><br> O que simplemente no les gusta hacer las cosas bien y el que termina pagando con su tiempo sos vos? <br> Con Sidekick esto no te sucederá nunca más!.</p>
             </div>
           </div>
         </div>
+    
+
+      <div class="card border-0">
+        <div class="row no-gutters">
+        <div class="col-xl-9 col-lg-9 col-md-12 col-xs-12">
+            <div class="card-body">
+            <p class="card-text">Por medio de Sidekick conocerás gente nueva, con los mismos intereses que vos. <br><br> Te sorprenderá la cantidad de buenos momentos que podes compartir con un desconocido, solamente hay que saber donde buscar!.</p>
+            </div>
+          </div>
+          <div class="col-xl-3 col-lg-3 col-md-12 col-xs-12">
+            <img class="img-fluid" alt="saving-time" src="img/coop.jpg">
+          </div>      
+        </div>
+      </div>
+
+      
+      <div class="card border-0">
+        <div class="row no-gutters">
+          <div class="col-xl-3 col-lg-3 col-md-12 col-xs-12">
+            <img class="img-fluid" alt="saving-time" src="img/karma.jpg">
+          </div>
+          <div class="col-xl-9 col-lg-9 col-md-12 col-xs-12">
+            <div class="card-body">
+            <p class="card-text">Con nuestro avanzado sistema de calificaciones que mezcla la Habilidad y el Karma podés tener una gran idea de lo que te vas a encontrar antes de invertir tu tan preciado tiempo. <br><br> Con Sidekick las sorpresas a la hora de jugar son cosa del pasado!.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+            
+      <div class="card border-0">
+        <div class="row no-gutters">
+        <div class="col-xl-9 col-lg-9 col-md-12 col-xs-12">
+            <div class="card-body">
+            <p class="card-text">Además, con nuestro sistema de calificaciones premium y premios por performance, el buen juego y las buenas actitudes son siempre recompensadas! <br><br> Sidekick apunta a tener una base sobre el comportamiento de todos los jugadores, y al hacer esto, reducimos la cantidad de jugadores con una mala actitud en toda la comunidad gamer!.</p>
+            </div>
+          </div>
+          <div class="col-xl-3 col-lg-3 col-md-12 col-xs-12">
+            <img class="img-fluid" alt="saving-time" src="img/medals.jpg">
+          </div>      
+        </div>
+      </div>
+
+    </div>
     </div>
 
     <div id="download">
-    <img class="img-fluid" src="img/separator.png" alt="separator">
+    <img class="img-fluid bg" src="img/separator.png" alt="separator">
       
-      <div class="card mb-3 border-0"  style="width:1500px; margin:0 auto;">
+      <div class="container my-5">
         <div class="row no-gutters">
 
 
@@ -99,26 +118,29 @@
 
         <div class="container my-5">
         <div class="row">
-          <div class="card col-md-3 text-center">
-              <img class="card-img-top" src="img/windows-logo.png" alt="windows"  height="194" width="162">
-              <br>
+          <div class="card col-xl-4 col-lg-4 col-md-4 col-xs-12  text-center">
               <div class="card-body">
+              <img class="card-img-top" src="img/windows-logo.png" alt="windows">
+              <br>
+              <br>
                 <a href="#" class="btn btn-primary">Descargar para Windows</a>
               </div>
             </div>
 
-            <div class="card col-md-3 text-center">
-              <img class="card-img-top" src="img/apple.png" alt="apple"  height="194" width="162">
-              <br>
+            <div class="card col-xl-4 col-lg-4 col-md-4 col-xs-12  text-center">
               <div class="card-body">
+              <img class="card-img-top" src="img/apple.png" alt="apple">
+              <br>
+              <br>
                 <a href="#" class="btn btn-primary">Descargar para iOS</a>
               </div>
             </div>
 
-            <div class="card col-md-3 text-center">
-              <img class="card-img-top" src="img/android.png" alt="android"  height="194" width="162">
-              <br>
+            <div class="card col-xl-4 col-lg-4 col-md-4 col-xs-12  text-center">
               <div class="card-body">
+              <img class="card-img-top" src="img/android.png" alt="android">
+              <br>
+              <br>
                 <a href="#" class="btn btn-primary">Descargar para Android</a>
               </div>
           </div>
@@ -127,26 +149,47 @@
       </div>
     </div>
 
-        <div class="container my-5" id="Info">
-          <h1>Info</h1>
-
+        <div class="container my-5" id="info">
                 
-        <div class="card mb-3 border-0" style="max-width: 688px;">
-          <div class="row no-gutters">
-            <div class="col-md-4">
-            <img src="img/gustavo.jpeg"  class="card-img" alt="gustavo" >
-            </div>
-            <div class="col-md-8">
-            <div class="card-body">
-                        <h5 class="card-title">Gustavo Ferreyra</h5>
-                        <p style="color: gray;">CEO</p>
-                        <p class="card-text">Tengo 21 años, soy de la provincia de Buenos Aires. Me apasionan los videojuegos, y aunque mayormente juego en solitario pensé que porque debe ser asi? Y al encontrarme insastifecho con las otras aplicaciones que hay en el mercado decidí hacer algo para cambiarlo. Y con SideKick pudimos hacerlo, y esperamos que todos puedan encontrarse satisfechos con esta plataformas y aprovechen su tiempo libre jugando al maximo.</p>
-                      </div>
+          <div class="card mb-3 border-0" style="max-width: 688px;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+              <img src="img/gustavo.jpeg"  class="card-img" alt="gustavo" >
+              </div>
+              <div class="col-md-8">
+              <div class="card-body">
+                          <h5 class="card-title">Gustavo Ferreyra</h5>
+                          <p style="color: gray;">CEO</p>
+                          <p class="card-text">Tengo 21 años, soy de la provincia de Buenos Aires. Me apasionan los videojuegos, y aunque mayormente juego en solitario pensé que porque debe ser asi? Y al encontrarme insastifecho con las otras aplicaciones que hay en el mercado decidí hacer algo para cambiarlo. Y con SideKick pudimos hacerlo, y esperamos que todos puedan encontrarse satisfechos con esta plataformas y aprovechen su tiempo libre jugando al maximo.</p>
+                        </div>
+              </div>
             </div>
           </div>
         </div>
-        </div>
 
+        <img class="img-fluid bg" src="img/separator-2.png" alt="separator">
+
+        <div class="container my-5" id="contact">
+           
+              <div class="row">
+                  <div class="inline">
+                            
+                    <div class="col-xl-4 col-lg-4 col-md-12 col-xs-12 inline">
+                        <ul>
+                          <li><i class="fa fa-street-view"></i> José María Paz 2039, Buenos Aires</li>
+                          <li><i class="fa fa-at"></i> ferreyragustavo847@gmail.com</li>
+                          <li><i class="fa fa-phone"></i> 11 2551-0623</li>
+                        </ul>  
+                        
+                    </div>
+
+                        <div class="col-xl-8 col-lg-8 col-md-12 col-xs-12 inline" >
+                            <img src="img/oficinas.jpg" alt="oficinas" class="img-fluid" height="440" width="686">
+                        </div>
+                    </div>
+                </div>          
+              </div>
+            </div>
 
     
 </body>
